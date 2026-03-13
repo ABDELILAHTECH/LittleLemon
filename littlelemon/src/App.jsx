8d271e67370e8ed50dcfaa731ee57880906c0cc3
abdelilah <abdelilahtech1@gmail.com>
2026-03-13 06:42:45 +0000

    add core SCSS structure: variables, reset, layout, main

--- littlelemon/src/App.jsx
+++ littlelemon/src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
+import './Styles/main.scss'
 import Header from './Components/Header'
 
 export default function App() {
   return (
-    <>
+    <div className="container">
       <Header />
-    </>
+    </div>
   )
 }
